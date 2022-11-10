7c233e3e63bb05a9ff7d48a48a270ea31bdbb0d2
amir241297 <amiretw2017@gmail.com>
2022-11-11 02:22:51 +0530

    Added Day-3 Task

---
+++ script/adminlogin0.2.js
@@ -0,0 +1,20 @@
+document.querySelector("form").addEventListener("submit",()=>{
+    event.preventDefault()
+    let name=document.getElementById("fn_299").value
+    let password=document.getElementById("pswd_299").value
+    testing(name,password)
+    
+})
+let flag=true
+let LSData=JSON.parse(localStorage.getItem("Admin"))
+const testing=(name,password)=>{
+    LSData.forEach(ele => {
+        if(name===ele.name && password===ele.password){
+            alert("Login Successfull!")
+            return flag=false
+        }
+    });
+    if(flag){
+        alert("Only Admin Can Login!")
+    }
+}
