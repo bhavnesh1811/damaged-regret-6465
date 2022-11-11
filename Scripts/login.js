a1b89dad20d8551bd8d54c444f0d82710e056c32
lokeshrautela25 <lokeshrautela25@gmail.com>
2022-11-12 00:43:40 +0530

    adding new updates

---
+++ Scripts/login.js
@@ -0,0 +1,56 @@
+import navbar from "../Components/navbar.js";
+console.log(navbar);
+let navbar_div=document.getElementById("navbar");
+navbar_div.innerHTML=navbar();
+
+import footer from '../Components/footer.js';
+    let footer_div=document.getElementById("footer");
+    footer_div.innerHTML=footer();
+
+let UserData = JSON.parse(localStorage.getItem("userData")) || [];
+// console.log(UserData);
+
+let otp = "123456";
+
+let verifyUser = document
+  .getElementById("verify")
+  .addEventListener("click", () => {
+    let giveErr = document.getElementById("err");
+    let otpAlert = document.getElementById("otpAlt");
+
+    let mobile_no = document.getElementById("mbl").value;
+    let userOTP = document.getElementById("OTP").value;
+
+    if (mobile_no == "" && userOTP == "") {
+      giveErr.style.visibility = "visible";
+      giveErr.innerText = "All Fields are Mandatory";
+      otpAlert.style.visibility = "visible";
+      otpAlert.style.color = "red";
+      otpAlert.innerText = "All Fields are Mandatory!";
+    }
+
+    UserData.forEach((el) => {
+      if (mobile_no !== el.mbl && mobile_no !== "") {
+        giveErr.style.visibility = "visible";
+        giveErr.innerText = "Unauthorised User***";
+      }
+       else if (userOTP !== el.OTP && userOTP !== "") {
+        otpAlert.style.visibility = "visible";
+        otpAlert.style.color = "red";
+        otpAlert.innerText = "Wrong OTP**";
+      } else if (mobile_no == el.mbl && userOTP == el.OTP && userOTP == otp) {
+        localStorage.setItem("user", JSON.stringify(el));
+        alert("Login In Successfully!");
+        window.location.href = "index.html";
+        // window.location.href = "userpage.html";
+      }
+
+    });
+    
+  });
+
+let resendOtp = document
+  .getElementById("resend")
+  .addEventListener("click", () => {
+    alert(`Your New OTP is ${otp}`);
+  });
