5471a74217dfaf09323f34387b2647d61dc543c0
bhavnesh1811 <erbhavnesharora@gmail.com>
2022-11-11 21:31:32 +0530

    coupon page

--- Scripts/payment.js
+++ Scripts/payment.js
@@ -1,4 +1,4 @@
-let urlData = "https://better-buy-rh4i.onrender.com/cartData";
+// let urlData = "https://better-buy-rh4i.onrender.com/cartData";
 let total=0;
 
 let deal_total=0;
@@ -8,26 +8,19 @@ totalAmount.innerHTML=null;
 //cartcount.innerHTML=null;
 let totalMrp=document.getElementById("totals")
 totalMrp.innerHTML=null;
-let cartTotal=async()=>{
-  let data=await fetch(urlData);
-  data=await data.json();
-  //console.log("data.length", data.length);
-
-  for(let i=0; i<data.length; i++){
-    total=total+data[i].actual_price;
-    deal_total=deal_total+data[i].deal_price;
-    //count++;
-  }
-  totalAmount.innerHTML=`Total Price :-${total}`;
-  totalMrp.innerHTML=`Deal Price :-${deal_total}`;
-  //cartcount.innerHTML=data.length;
-  localStorage.setItem("cart_total", JSON.stringify(deal_total))
+let cartTotal=()=>{
+  let data=localStorage.getItem("cart_total");
+  console.log('data:', data)
+  
+  totalAmount.innerHTML=`Total Amount to be Paid :-${data}`;
+  
 }
 cartTotal();
 const paymentproceed=(e)=>{
     e.preventDefault();
     setTimeout(() => {
         alert("Your order is Placed Successfully, Thanks for Shopping!!") ;
+        // let data=localStorage.setItem("cart_total",0);
         window.location.href="index.html";
     }, 2000);
   
