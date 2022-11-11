82d931219e8a2a9db90ecffd1e667871400ac707
lokeshrautela25 <lokeshrautela25@gmail.com>
2022-11-12 05:10:42 +0530

    adding new files

---
+++ Scripts/fashion.js
@@ -0,0 +1,10 @@
+import navbar from "../Components/navbar.js";
+import footer from "../Components/footer.js";
+let navbar_div = document.getElementById("navbar");
+navbar_div.innerHTML = navbar();
+let footer_div = document.getElementById("footer");
+footer_div.innerHTML = footer();
+
+
+
+
