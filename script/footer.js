6a7ca4fd35f446b133631eeceffd32418c1a7401
gauravfar93 <gauravfar93@gmail.com>
2022-11-11 16:05:55 +0530

    added footer

---
+++ script/footer.js
@@ -0,0 +1,43 @@
+function footer() {
+    return `<div id="footer_box">
+    <div id="footer1">
+        <h2>MOST POPULAR CATEGORIES</h2>
+        <h3>Staples</h3>
+        <h3>Beverages</h3>
+        <h3>Personal Care</h3>
+        <h3>Home Care</h3>
+        <h3>Fruits and Vegetables</h3>
+        <h3>Baby Care</h3>
+        <h3>Snacks & Branded Foods</h3>
+        <h3>Dairy & Bakery</h3>
+    </div>
+    <div id="footer2">
+        <h2>CUSTOMER SERVICES</h2>
+        <h3>About Us</h3>
+        <h3>FAQ</h3>
+        <h3>Terms and conditions</h3>
+        <h3>Privacy policy</h3>
+        <h3>E-waste Policy</h3>
+        <h3>Cancellation and return policy</h3>
+        
+    </div>
+    <div id="footer3">
+        <h2>CONTACT US</h2>
+        <h3>WhatsApp us : 70003 70003</h3>
+        <h3>Call Us : 1800 890 1222</h3>
+        <h3>Please note that you are accessing the BETA Version of www.jiomart.com</h3>
+        <h3>Should you encounter any bugs, glitches, lack of functionality, delayed deliveries, billing errors or other problems on the beta website, please email us on cs@jiomart.com</h3>
+        <h2>DOWNLOAD APP</h2>
+        <img src="./image/Buy-Grocery-Online-Daily-Needs-Supermarket-JioMart.png" alt="">
+    </div>
+</div>
+<div id="bottom_footer">
+    <hr>
+    <div id="bottom_f">
+        <h3>Best viewed on Microsoft Edge 81+, Mozilla Firefox 75+, Safari 5.1.5+, Google Chrome 80+</h3>
+        <h3>© 2022 All rights reserved. Reliance Retail Ltd.</h3>
+    </div>
+</div>`
+  }
+  export default footer;
+  
