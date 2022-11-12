f510598a2eefd33754c1f829ac5b22013da7a6d5
LOKESH <107460679+lokeshrautela25@users.noreply.github.com>
2022-11-13 02:06:16 +0530

    Update fashion.js

--- Scripts/fashion.js
+++ Scripts/fashion.js
@@ -39,19 +39,12 @@ const appendTop = async () => {
     let prod_title = document.createElement('h2');
     prod_title.innerHTML = `${title}`;
 
-    //   let prod_ratings = document.createElement('p');
-    //   prod_ratings.innerHTML = `Ratings:-${ratings}`;
-
     let prod_deal_price = document.createElement('b');
     prod_deal_price.innerHTML = `Deal Price:- ₹ ${deal_price}`
 
     let prod_actual_price = document.createElement('p');
     prod_actual_price.innerHTML = `M.R.P:- ₹ ${actual_price}`;
     prod_actual_price.style.textDecoration = "line-through"
-
-    //   let savings = document.createElement('p');
-    //   savings.innerHTML = `Savings:- ₹ ${actual_price - deal_price}`;
-    //   savings.style.textDecoration = "none"
 
     let btn = document.createElement('button');
     btn.innerHTML = 'Add to cart';
@@ -99,19 +92,12 @@ const sortedData = async (data) => {
     let prod_title = document.createElement('h2');
     prod_title.innerHTML = `${title}`;
 
-    //   let prod_ratings = document.createElement('p');
-    //   prod_ratings.innerHTML = `Ratings:-${ratings}`;
-
     let prod_deal_price = document.createElement('b');
     prod_deal_price.innerHTML = `Deal Price:- ₹ ${deal_price}`
 
     let prod_actual_price = document.createElement('p');
     prod_actual_price.innerHTML = `M.R.P:- ₹ ${actual_price}`;
     prod_actual_price.style.textDecoration = "line-through"
-
-    //   let savings = document.createElement('p');
-    //   savings.innerHTML = `Savings:- ₹ ${actual_price - deal_price}`;
-    //   savings.style.textDecoration = "none"
 
     let btn = document.createElement('button');
     btn.innerHTML = 'Add to cart';
@@ -160,6 +146,64 @@ sortByPriceLH.onclick = async () => {
   console.log('data:', data);
   sortedData(data);
 }
+
+
+// datadiv 2 append files 
+
+
+const appendTops = async () => {
+  let best_seller = document.querySelector('#dataDiv2');
+  best_seller.innerHTML = null;
+  
+
+  let data = await fetch(`${url}`);
+  data = await data.json();
+  data.forEach(({ url, title, deal_price, actual_price, id }) => {
+
+    let div = document.createElement('div');
+    div.className = 'items'
+
+    let prod_image = document.createElement('img');
+    prod_image.src = `${url}`;
+
+    let prod_title = document.createElement('h2');
+    prod_title.innerHTML = `${title}`;
+    let prod_deal_price = document.createElement('b');
+    prod_deal_price.innerHTML = `Deal Price:- ₹ ${deal_price}`
+
+    let prod_actual_price = document.createElement('p');
+    prod_actual_price.innerHTML = `M.R.P:- ₹ ${actual_price}`;
+    prod_actual_price.style.textDecoration = "line-through"
+      
+    let btn = document.createElement('button');
+    btn.innerHTML = 'Add to cart';
+    btn.addEventListener("click", async () => {
+      let urlData = "https://better-buy-rh4i.onrender.com/cartData";
+      let cartData = {
+        url,
+        title,
+        deal_price,
+        actual_price,
+        id: Date.now() + Math.random(),
+      }
+      cartData = JSON.stringify(cartData);
+
+      await fetch(urlData, {
+        method: "POST",
+        body: cartData,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      dataCount();
+    });
+
+
+    div.append(prod_image, prod_title, prod_deal_price, prod_actual_price, btn)
+    best_seller.append(div);
+  })
+}
+appendTops();
 
 
 
